6f84139211c3a9d110a4cad5f5a68b06e7ab59f5
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2022-04-26 16:26:39 +0100

    many improvements

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ server = Flask(__name__)
 @server.route('/index') 
 def home(): 
 	movies = []
-	ids = ['tt3896198', 'tt0111161', 'tt0499549', 'tt0082971', 'tt0325980']
+	ids = ['tt3896198', 'tt0111161', 'tt0499549', 'tt0082971', 'tt0325980', 'tt0295297']
 	for id in ids: 
 		r = requests.get(f"http://www.omdbapi.com/?i={id}&apikey=d9ba51a5")
 		data = r.json()
